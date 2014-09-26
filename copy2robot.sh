7fee454234f09936e818d0d1b8d7d35fefd8049f
Adam Bajric <adam.bajric@ntb.ch>
2014-09-26 15:58:08 +0200

    copy test programs to robot

--- copy2robot.sh
+++ copy2robot.sh
@@ -10,4 +10,10 @@ cp build-armhf/eeros/src/libeeros.so $tmpdir/lib
 cp build-armhf/eeduro/delta/eeduro-delta $tmpdir/bin
 cp build-armhf/eeduro/util/eeduro-ctrl $tmpdir/bin
 
+cp build-armhf/eeduro/test/led $tmpdir/bin
+cp build-armhf/eeduro/test/button $tmpdir/bin
+cp build-armhf/eeduro/test/encoder $tmpdir/bin
+cp build-armhf/eeduro/test/magnet $tmpdir/bin
+cp build-armhf/eeduro/test/voltage $tmpdir/bin
+
 scp -r $tmpdir/bin $tmpdir/lib root@192.168.7.2:/opt/eeduro-0.3/
