738e89a6bf820c6544634a425337ca6313007f3d
ursgraf <urs.graf@ntb.ch>
2019-11-15 17:02:54 +0100

    Change scripts for version checking of libraries

--- delta/clone.sh
+++ delta/clone.sh
@@ -5,29 +5,27 @@ script_dir="$(dirname $script)"
 
 . "$script_dir/config.sh.in"
 
-git clone $roboticscape_github_address -o upstream "$roboticscape_dir"
-pushd "$roboticscape_dir"
-git checkout $roboticscape_github_version
-popd
-
-
 if [ ! -d "$eeros_source_dir" ]; then
-	git clone $eeros_github_address -o upstream "$eeros_source_dir"
+	git clone $eeros_git_remote_address -o upstream "$eeros_source_dir"
 	pushd "$eeros_source_dir"
-	git checkout $eeros_github_version
+	git checkout $eeros_git_version
 	popd
 fi
 
+if [ ! -d "$librobotcontrol_source_dir" ]; then
+	git clone "$librobotcontrol_git_remote_address" -o upstream "$librobotcontrol_source_dir"
+	pushd "$librobotcontrol_source_dir"
+	git checkout "$librobotcontrol_git_version"
+	popd
+fi
 
-if [ ! -z ${bbblue_eeros_source_dir+y} ]; then
-	if [ ! -d "$bbblue_eeros_source_dir" ]; then
-		git clone $bbb_eeros_github_address -o upstream "$bbblue_eeros_source_dir"
-		pushd $bbblue_eeros_source_dir
-		git checkout $bbb_eeros_github_version
-		popd
-	fi
+if [ ! -d "$bbblue_eeros_source_dir" ]; then
+	git clone "$bbblue_eeros_git_remote_address" -o upstream "$bbblue_eeros_source_dir"
+	pushd "$bbblue_eeros_source_dir"
+	git checkout "$bbblue_eeros_git_version"
+	popd
 fi
 
 if [ ! -d "$delta_source_dir" ]; then
-	git clone $eeduro_delta_address -o upstream "$delta_source_dir"
+	git clone $delta_git_remote_address -o upstream "$delta_source_dir"
 fi
--- delta/make.sh
+++ delta/make.sh
@@ -5,6 +5,10 @@ script_dir="$(dirname $script)"
 
 . "$script_dir/config.sh.in"
 
+if [ "$rm_build_install_dir" = true ]; then
+  rm -rf "$build_dir" "$install_dir"
+fi
+
 
 mkdir -p $eeros_build_dir
 pushd $eeros_build_dir
@@ -17,30 +21,30 @@ make install
 popd
 
 
-if [ ! -z ${bbblue_eeros_source_dir+x} ]; then
-  mkdir -p $bbblue_eeros_build_dir
-  pushd $bbblue_eeros_build_dir
-  cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
-        -DADDITIONAL_INCLUDE_DIRS="$roboticscape_dir/libraries/" \
-        -DADDITIONAL_LINK_DIRS="$roboticscape_dir/libraries/" \
-        -DCMAKE_INSTALL_PREFIX=$install_dir \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DREQUIRED_EEROS_VERSION=$eeros_required_version \
-	$bbblue_eeros_source_dir
-  make
-  make install
-  popd
-fi
+mkdir -p $bbblue_eeros_build_dir
+pushd $bbblue_eeros_build_dir
+cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
+      -DADDITIONAL_INCLUDE_DIRS="$librobotcontrol_source_dir/libraries/" \
+      -DADDITIONAL_LINK_DIRS="$librobotcontrol_source_dir/libraries/" \
+      -DCMAKE_INSTALL_PREFIX=$install_dir \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DREQUIRED_EEROS_VERSION=$eeros_required_version \
+      $bbblue_eeros_source_dir
+make
+make install
+popd
 
 
 mkdir -p $delta_build_dir
 pushd $delta_build_dir
-cmake	-DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
-      -DADDITIONAL_INCLUDE_DIRS="$roboticscape_dir/libraries/" \
-      -DADDITIONAL_LINK_DIRS="$roboticscape_dir/libraries/" \
+cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
+      -DADDITIONAL_INCLUDE_DIRS="$librobotcontrol_source_dir/libraries/" \
+      -DADDITIONAL_LINK_DIRS="$librobotcontrol_source_dir/libraries/" \
       -DCMAKE_INSTALL_PREFIX=$install_dir \
       -DCMAKE_BUILD_TYPE=Release \
-      -DREQUIRED_EEROS_VERSION=$eeros_required_version \
-	$delta_source_dir
+      -DUSE_BBBLUE="$use_bbblue" \
+      -DREQUIRED_EEROS_VERSION="$eeros_required_version" \
+      -DREQUIRED_BBBLUE_EEROS_VERSION="$bbblue_eeros_required_version" \
+      $delta_source_dir
 make
 popd
