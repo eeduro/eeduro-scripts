15a4b9447855494240fd21511726bb0fbdbea9b4
ursgraf <urs.graf@ost.ch>
2021-04-23 14:39:09 +0200

    Use SDK for ARM

--- delta/clone.sh
+++ delta/clone.sh
@@ -5,26 +5,6 @@ script_dir="$(dirname $script)"
 
 . "$script_dir/config.sh.in"
 
-if [ ! -d "$eeros_source_dir" ]; then
-	git clone $eeros_git_remote_address -o upstream "$eeros_source_dir"
-	pushd "$eeros_source_dir"
-	git checkout $eeros_git_version
-	popd
-fi
-
-if [ ! -d "$librobotcontrol_source_dir" ]; then
-	git clone "$librobotcontrol_git_remote_address" -o upstream "$librobotcontrol_source_dir"
-	pushd "$librobotcontrol_source_dir"
-	git checkout "$librobotcontrol_git_version"
-	popd
-fi
-
-if [ ! -d "$bbblue_eeros_source_dir" ]; then
-	git clone "$bbblue_eeros_git_remote_address" -o upstream "$bbblue_eeros_source_dir"
-	pushd "$bbblue_eeros_source_dir"
-	git checkout "$bbblue_eeros_git_version"
-	popd
-fi
 
 if [ ! -d "$delta_source_dir" ]; then
 	git clone $delta_git_remote_address -o upstream "$delta_source_dir"
--- delta/make.sh
+++ delta/make.sh
@@ -5,46 +5,43 @@ script_dir="$(dirname $script)"
 
 . "$script_dir/config.sh.in"
 
+MAKE="make -j$(nproc)"
+
 if [ "$rm_build_install_dir" = true ]; then
-  rm -rf "$build_dir" "$install_dir"
+  rm -rf "$delta_build_dir" "$install_dir"
 fi
 
+# build sourceDir buildDir cmakeFlags
+function build ()
+{
+  local source_dir="$1"
+  local build_dir="$2"
+  shift 2
+  local flags="$@"
 
-mkdir -p $eeros_build_dir
-pushd $eeros_build_dir
-cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
-      -DCMAKE_INSTALL_PREFIX=$install_dir \
-      -DCMAKE_BUILD_TYPE=Release \
-      $eeros_source_dir
-make
-make install
-popd
+echo "aaa: $wd"
+echo "bbb: $source_dir $build_dir"
 
+  mkdir -p "$build_dir"
+  pushd "$build_dir"
 
-mkdir -p $bbblue_eeros_build_dir
-pushd $bbblue_eeros_build_dir
-cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
-      -DADDITIONAL_INCLUDE_DIRS="$librobotcontrol_source_dir/libraries/" \
-      -DADDITIONAL_LINK_DIRS="$librobotcontrol_source_dir/libraries/" \
-      -DCMAKE_INSTALL_PREFIX=$install_dir \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DREQUIRED_EEROS_VERSION=$eeros_required_version \
-      $bbblue_eeros_source_dir
-make
-make install
-popd
+  cmake -DCMAKE_INSTALL_PREFIX="$install_dir" \
+        -DCMAKE_BUILD_TYPE=Release \
+        $flags \
+        "$source_dir"
+
+  $MAKE
+  popd
+}
 
 
-mkdir -p $delta_build_dir
-pushd $delta_build_dir
-cmake -DCMAKE_TOOLCHAIN_FILE=$toolchain_file \
-      -DADDITIONAL_INCLUDE_DIRS="$librobotcontrol_source_dir/libraries/" \
-      -DADDITIONAL_LINK_DIRS="$librobotcontrol_source_dir/libraries/" \
-      -DCMAKE_INSTALL_PREFIX=$install_dir \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DUSE_BBBLUE="$use_bbblue" \
-      -DREQUIRED_EEROS_VERSION="$eeros_required_version" \
-      -DREQUIRED_BBBLUE_EEROS_VERSION="$bbblue_eeros_required_version" \
-      $delta_source_dir
-make
-popd
+#if [ "$use_cross_compilation_environment" = true ]; then
+  unset LD_LIBRARY_PATH
+  . "$environment_setup_script"
+  install_dir="$SDKTARGETSYSROOT/$sdk_install_dir"
+  echo "Due to cross compilation environment, install_dir is set to: $install_dir"
+#fi
+
+
+build "$delta_source_dir" "$delta_build_dir" -DREQUIRED_EEROS_VERSION="$eeros_required_version" 
+  
